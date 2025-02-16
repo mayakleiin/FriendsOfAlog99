b19802ab9f6ebab29ae307cbe6359cff219abc9f
mayakleiin <mayaklein1999@cs.colman.ac.il>
2025-02-16 21:35:12 +0200

    Fixed sitemap and robots.txt accessibility in vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, PluginOption } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+function htmlPlugin(): PluginOption {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      return html.replace(
+        "</head>",
+        `
+        <meta name="description" content="עמותת ידידי החטיבה הטכנולוגית - תמיכה ותרומות לאוגדה 99">
+        <meta name="keywords" content="אלוג 99, אוגדה 99, חטיבה טכנולוגיסטית, יחידה טכנולוגית, תרומות">
+        <meta name="author" content="Friends of Alog 99">
+        <meta property="og:title" content="אלוג 99 - עמותת ידידי החטיבה הטכנולוגית">
+        <meta property="og:description" content="תמכו בטכנולוגיות החדשניות של אוגדה 99 - יחידה טכנולוגיסטית מובילה.">
+        <meta property="og:image" content="https://alog-web.vercel.app/images/logo.png">
+        <meta property="og:url" content="https://alog-web.vercel.app">
+        <meta property="og:type" content="website">
+        </head>`
+      );
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), htmlPlugin()],
+});
