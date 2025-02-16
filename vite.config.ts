d976fa2434b75ec752eb7142be0928e4dcad1c6d
mayakleiin <mayaklein1999@cs.colman.ac.il>
2025-02-17 00:18:10 +0200

    updated meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ function htmlPlugin(): PluginOption {
         `
         <meta name="keywords" content="אלוג 99, אלו"ג 99, אלוג, אלו"ג, אוגדה 99, חטיבה טכנולוגיסטית, ידידי החטיבה הטכנולוגיסטית">
         <meta name="author" content="Friends of Alog 99">
-        <meta property="og:title" content= "עמותת ידידי החטיבה הטכנולוגיסטית - אלו"ג 99">
+        <meta property="og:title" content= "עמותת ידידי החטיבה הטכנולוגיסטית">
+        <meta property="og:description" content= "אלו"ג 99">
         <meta property="og:image" content="https://alog99.com/images/logo.png">
         <meta property="og:url" content="https://alog99.com">
         <meta property="og:type" content="website">
